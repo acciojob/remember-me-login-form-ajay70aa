faf21501b4c31e0d6d812343e0604f5a3351dc35
D. Ajay Kumar <154305661+ajay70aa@users.noreply.github.com>
2025-05-07 11:19:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 //your JS code here. If required.
+var username = document.getElementById('username')
+var password = document.getElementById('password')
+var checkbox = document.getElementById('checkbox')
+var submitBtn = document.getElementById('submit')
+var existingUser = document.getElementById('existing')
+const formContainer = document.getElementById('form-container')
+
+window.onload = function (){
+	const savedUser = localStorage.getItem("userObj")
+	if(savedUser){
+		existingUser.style.display = "block"
+	}
+}
+function handleCredentials(e){
+	e.preventDefault()
+	alert(`Logged in as ${username.value}`)
+	if(checkbox.checked){
+		let userObj = {
+			username: username.value,
+			password: password.value
+		}
+		localStorage.setItem("userObj", JSON.stringify(userObj))
+	} else {
+		localStorage.removeItem("userObj")
+	}
+	formContainer.style.display = "none"
+	if(localStorage.getItem("userObj")){
+		existingUser.style.display = "block";
+	}
+}
+
+existingUser.addEventListener('click', function(){
+	const saved = JSON.parse(localStorage.getItem('userObj'))
+	if(saved){
+		alert("Logged in as ${saved.username}")
+		formContainer.style.display = "none"
+	}
+})
+
+submitBtn.addEventListener('click', handleCredentials)
+
+
